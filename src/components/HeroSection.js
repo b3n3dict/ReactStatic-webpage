9d476af17c316aac65f9be018f9cac8442adf8aa
Benedict Xavier <benedict.xavier.e@gmail.com>
2021-05-09 01:35:25 +0530

    project finished

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -7,7 +7,7 @@ const HeroSection = () => {
         <div className="hero-container">
         <video src="./videos/video-1.mp4" autoPlay loop muted/>
         <h1>ADVENTURE AWAITS</h1>
-            <p>What are you waiting for?</p>
+            <p>What are you waiting for.?</p>
             <div className="hero-btns">
                 <Button className="btns" buttonStyle='btn--outline' buttonSize='btn-large'>GET STARTED</Button>
                 <Button className="btns" buttonStyle='btn--primary' buttonSize='btn-large'>
