71e28f6b229b9e74542c0e40fb20820bac875125
Benedict Xavier <benedict.xavier.e@gmail.com>
2021-05-09 01:08:23 +0530

    react-static-page-finished

---
+++ src/components/pages/Home.js
@@ -0,0 +1,15 @@
+import '../../App.css'
+import Cards from '../Cards'
+import Footer from '../Footer'
+import HeroSection from '../HeroSection'
+
+
+export  default function  Home(){
+    return(
+        <>
+         <HeroSection/>
+         <Cards/>
+         <Footer/>
+        </>
+    )
+}
